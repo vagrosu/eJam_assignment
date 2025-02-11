ccda2652cab4da6705375d23434c371adc5cbb50
vagrosu <vagrosu@yahoo.com>
2025-02-11 20:15:38 +0200

    Add new superheroes

--- backend/database/storage.ts
+++ backend/database/storage.ts
@@ -46,6 +46,54 @@ const storage: StorageType = {
       superpower: "Agility",
       humilityScore: 3,
     },
+    {
+      id: "p9z4vhr8dvhi0hpp1dmqzjst",
+      name: "Supernova",
+      superpower: "Strength",
+      humilityScore: 4,
+    },
+    {
+      id: "q3h9rklaeb112xlou8url4dr",
+      name: "Titan",
+      superpower: "Durability",
+      humilityScore: 6,
+    },
+    {
+      id: "u04v3svb0nuwfh77rceux2gj",
+      name: "Velocity",
+      superpower: "Speed",
+      humilityScore: 10,
+    },
+    {
+      id: "t9cb7wfgdnqijpz2dcnfbiqx",
+      name: "Steelheart",
+      superpower: "Agility",
+      humilityScore: 2,
+    },
+    {
+      id: "aocaqa5z8ixz71g1786tqpz2",
+      name: "Nightwatch",
+      superpower: "Strength",
+      humilityScore: 1,
+    },
+    {
+      id: "qf2u4mr2euie8j53kufu7gfy",
+      name: "Verdant",
+      superpower: "Regeneration",
+      humilityScore: 10,
+    },
+    {
+      id: "uw1akgm6iyf704mcqnwmlc1k",
+      name: "Arachnid",
+      superpower: "Agility",
+      humilityScore: 3,
+    },
+    {
+      id: "uw1akgm2iyd404mcqnwflc1k",
+      name: "Cat Woman",
+      superpower: "Agility",
+      humilityScore: 7,
+    },
   ],
 };
 
